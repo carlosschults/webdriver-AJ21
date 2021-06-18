c308cbc113cc24d20d20b9926dba44b60b1a8950
Carlos Schults <carlos.schults@gmail.com>
2021-06-17 21:27:10 -0300

    Add Selenium test

--- src/WebDriverDemo/Program.cs
+++ src/WebDriverDemo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,11 @@ namespace WebDriverDemo
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://google.com");
+            IWebElement element = driver.FindElement(By.Name("q"));
+            element.SendKeys("testes automatizados");
+            element.SendKeys(Keys.Return);
         }
     }
 }
